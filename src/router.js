221b84a83f060a7c48d3ae641c1daaabe99ab268
pbywater <philippabywater@gmail.com>
2017-03-15 16:50:01 +0000

    changing html input fields ready for autocomplete
    relates to #1

--- src/router.js
+++ src/router.js
@@ -1,17 +1,14 @@
 // const fs = require('fs');
 // const path = require('path');
 const handlers = require('./handler');
-module.exports = function(request, response) {
-  var url = request.url;
-  var extension = url.split('.')[1];
-    if (url === "/") {
-      handlers.serveLanding(request, response);
-  }
-  else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico') {
+module.exports = function (request, response) {
+  const url = request.url;
+  const extension = url.split('.')[1];
+  if (url === '/') {
+    handlers.serveLanding(request, response);
+  } else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico') {
     handlers.servePublic(request, response);
-  }
-
-  else {
+  } else {
     handlers.pageNotFound(request, response);
   }
 };
