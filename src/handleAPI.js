0f38932d909b86ca2ca5d07e3111be0f29b87505
oliverjam <oliverjamesphillips@gmail.com>
2017-03-16 10:19:55 +0000

    Use the correct header stupid
    Relates #29

--- src/handleAPI.js
+++ src/handleAPI.js
@@ -1,5 +1,5 @@
 function handleAPI(response, api) {
-  response.writeHead(200, { 'content-type': 'application/json', 'application-control-origin': '*' });
+  response.writeHead(200, { 'content-type': 'application/json', 'access-control-allow-origin': '*' });
   response.end(JSON.stringify(api));
 }
 
