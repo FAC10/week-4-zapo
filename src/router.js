aa68f68c82131cd0c337960826de43ab4b31a9a0
Zooey <zooey@lbk.org.uk>
2017-03-16 11:22:30 +0000

    add beginnings of request filter We've begun by taking the querystring and making it into an object so that it is easier to deal with relates to #21

--- src/router.js
+++ src/router.js
@@ -3,18 +3,28 @@
 const handlers = require('./handler');
 
 module.exports = function (request, response) {
-
   const url = request.url;
   const extension = url.split('.')[1];
   if (url === '/') {
     handlers.serveLanding(request, response);
   } else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico') {
     handlers.servePublic(request, response);
+  } else if (url.indexOf('/api/words') !== -1) {
+    url = url.replace('/api/words/?', '').split('&');
 
-  } else if (url === '/api/words') {
+    const dataHolder = {};
+
+    myUrl = myUrl.forEach((e) => {
+      const keyValue = e.split('=');
+      dataHolder[a[0]] = a[1];
+    });
+
+
+    console.log(obj);
+
+    console.log(url);
 
     handlers.handleAPI(response, { id: '4', words: ['aardvark', 'abacus', 'Aberdeen', 'Aldine'] });
-
   } else {
     handlers.pageNotFound(request, response);
   }
