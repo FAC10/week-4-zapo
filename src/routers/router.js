484cfe6a559a7d2d525643683624d4638950cf35
oliverjam <oliverjamesphillips@gmail.com>
2017-03-17 12:43:32 +0000

    Remove handling logic from router
    Ensure our router and handler always take consistent arguments
    
    Relates #59

--- src/routers/router.js
+++ src/routers/router.js
@@ -9,7 +9,7 @@ module.exports = function (request, response) {
   } else if (extension === 'css' || extension === 'js' || extension === 'html' || extension === 'ico') {
     handlers.servePublic(request, response);
   } else if (url.indexOf('/api/words') !== -1) {
-    handlers.serveAPI(response, handleAPI(url));
+    handlers.serveAPI(request, response);
   } else {
     handlers.pageNotFound(request, response);
   }
